cd8d487260a2683b953f89ba40dfeecfee11f041
Murat YavuzKAPLAN <murat@yavuzkaplan.com>
2021-08-21 00:59:43 +0300

    Renamed an ugly method name :)

--- Storage.MongoDB/Storage.MongoDB/Extentions/ResultExtentions.cs
+++ Storage.MongoDB/Storage.MongoDB/Extentions/ResultExtentions.cs
@@ -4,7 +4,7 @@ namespace Back.Zone.Storage.MongoDB.Extentions
 {
     public static class ResultExtentions
     {
-        public static bool IsExists(this UpdateResult updateResult) =>
+        public static bool HasSucceed(this UpdateResult updateResult) =>
             updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
     }
 }
