81ccad3d35524cffb78b68dedbbd82421d18838a
Murat YavuzKAPLAN <murat@yavuzkaplan.com>
2022-05-27 22:47:23 +0300

    ResultExtensions bug fixed

--- Storage.MongoDB/Storage.MongoDB/Extensions/ResultExtensions.cs
+++ Storage.MongoDB/Storage.MongoDB/Extensions/ResultExtensions.cs
@@ -5,7 +5,7 @@ namespace Back.Zone.Storage.MongoDB.Extensions;
 public static class ResultExtensions
 {
     public static bool Succeed(this UpdateResult updateResult) =>
-        updateResult.IsAcknowledged && updateResult.ModifiedCount >= 0;
+        updateResult.IsAcknowledged && updateResult.MatchedCount > 0 && updateResult.ModifiedCount >= 0;
 
     public static bool Succeed(this DeleteResult deleteResult) =>
         deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
